46bcff1198b21d6d037900695c4efdd0574392f0
mwaller129 <mwaller129@gmail.com>
2023-05-08 16:22:19 -0400

    push 4

--- main.js
+++ main.js
@@ -1,50 +1,74 @@
 /*----- constants -----*/
-const answers = [
+const board = [
     [1, 0, 0,],  // col 0
     [0, 0, 0,],  // col 1
     [0, 0, 0,],  // col 2
-    [0, 0, 0,]  // col 3
+    [0, 1, 0,]  // col 3
   ];
 
-  const questions = []
+  const questions = ['5 x 3', '5 x 7', '5 x 10', '5 x 4', '5 x 1', '5 x 5', '5 x 11', '5 x 2', '5 x 12', '5 x 9', ' 5 x 6', '5 x 8']
 
 
 /*----- state variables -----*/ 
 let gameComplete; //game in play or user has completed game
-// set Timeout to make question change 
-
+let evaluate; // isCorrect or is Incorrect
+let player = 1
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('q1');
+const boardEls = document.getElementById(board);
 
 /*----- event listeners -----*/
-
+document.getElementById('questions').addEventListener('click', handleClick);
+document.getElementById('board').addEventListener('click', makeChoices);
 
 /*----- functions -----*/
-const init = () => {
-  complete = null;
+init = () => {
+  gameComplete = null;
   render();
+
+};
+//boardEls.forEach((, colIdx) => {
+  //  const hideCell = (!board[colIdx].isCorrect || gameComplete === true)
+    //   evaluate =`${c0r1}` === true
+//})
+
+function makeChoices(evt) {
+    const rowIdx = board.indexOf(evt.target);
+    console.log(evt.target);
+    render();
 }
 
-render => { //display full board
-    renderAnswers();
+
+function renderBoard() {
+   board.forEach((colArr, colIdx) => {
+        colArr.forEach((cellval, rowIdx) => {
+            const cellId = `c${colIdx}r${rowIdx}`;
+            const cellEl = document.getElementById(cellId);
+        });
+       
+        });
+        
+    }
+
+
+function renderDone() {
+    if (gameComplete === 'C') {
+        messageEl.innerText = "Congratulations!" 
+    } else {
+        messageEl.innerText = 'questions[1]';
+
+    }
+}
+
+function renderControls()    {
+    startAgainBtn.style.visibility = done ? 'visible' : 'hidden';
+}
+
+
+
+render = () => { //display full board
+    renderBoard();
     renderDone();//congratulatory messgae when player finsihes
     renderControls();
 
 }
-
-renderAnswers => {
-    answers.forEach((ansIdx, colArr) => {
-       // if ansInd === match for equation in header, answer is hidden
-        });
-        
-    };
-
-
-function renderDone() {
-
-}
-
-function renderControls()    {
-
-}
-
