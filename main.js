a0c9951495c36f03b8dad823c631473b365c6bca
mwaller129 <mwaller129@gmail.com>
2023-05-08 02:05:07 -0400

    push 2

---
+++ main.js
@@ -0,0 +1,22 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+function init() {
+board = [
+    [0, 0, 0,],  // col 0
+    [0, 0, 0,],  // col 1
+    [0, 0, 0,],  // col 2
+    [0, 0, 0,]  // col 3
+]
+}
