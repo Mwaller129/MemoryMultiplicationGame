7639a7f57d1743501cdb93c4450e8c8dfbcbc604
mwaller129 <mwaller129@gmail.com>
2023-05-10 02:24:39 -0400

    push 7

--- main.js
+++ main.js
@@ -17,13 +17,44 @@ let player = 1
 let fact = ''
 let answer = ''
 let choice = 0
+let count = 0
 /*----- cached elements  -----*/
 //const messageEl = document.querySelector('q1');
-const boardEls = document.getElementById('div');
-
+//const boardEls = document.getElementById('div');
+  const set = () => {
+    let evaluate = document.querySelectorAll('.evaluate')
+    evaluate.forEach((tile) => {
+    tile.classList.add('set')
+})
+}
 /*----- event listeners -----*/
 //document.getElementById('questions').addEventListener('click', handleClick);
-document.getElementById('board').addEventListener('click', makeChoices);
+document.getElementById('board').addEventListener('click', function(evt) {
+    let choice = evt.target
+    // if (choice === 'div') {
+    //     return
+    // }
+    choice.classList.add('evaluate')
+    console.log(evt.target);
+    //render();
+if (count < 2) {
+    count++
+    if (count === 1) {
+       fact = choice.dataset.name
+       choice.classList.add('evaluate')
+    } else {
+        answer = choice.dataset.name
+        choice.classList.add('evaluate')
+    }
+     if (fact !== '' && answer !== '') {
+        if (fact === answer) {
+            set();
+        }
+    }
+}
+})  
+//}
+
 
 /*----- functions -----*/
 init = () => {
@@ -31,82 +62,93 @@ init = () => {
   render();
 
 };
+board.forEach((item) => {
+    const tile = document.createElement('div')
+    tile.classList.add('tile')    
+    tile.classList.name = item.name
+    
+})
+// function makeChoices(evt) {
+//     let choice = evt.target
+//     if (choice === 'div'){
+//         return
+//     }
+//     choice.classList.add('choice')
+//     console.log(evt.target);
+    //render();
+//}
+item = () => {
+choice.dataset.name = item.name
+    let item = document.getElementById('div')
+}
+//  const set = () => {
+//      let evaluate = document.querySelectorAll('.evaluate')
+//      evaluate.forEach((tile) => {
+//      tile.classList.add('set')
+//  })
+//  }
+
+
+// board.addEventListener('click', function (event) {
+//     let choice = evt.target
+//     if (choice === 'div'){
+//         return
+//     }
+//     choice.classList.add('choice')
+//     console.log(evt.target);
+//     //render();
+// if (count < 2) {
+//     count++
+//     if (count === 1) {
+//        fact = choice.dataset.name
+//        choice.classList.add('choice')
+//     } else {
+//         answer = choice.dataset.name
+//         choice.classList.add('choice')
+//     }
+//     if (fact !== '' && answer !== '') {
+//         if (fact === answer) {
+//             set()
+//         }
+//     }
+// }
+// })
 
 render = () => { //display full board
     renderBoard();
     renderDone();//congratulatory messgae when player finsihes
     //renderControls();
 
-}
-
- board.forEach((item) => {
-     const tile = document.createElement('div')
-     tile.classList.add('tile')    
-     tile.classList.name = item.name
-     
- })
- 
-function makeChoices(evt) {
-    let choice = evt.target
-    if (choice === 'div'){
-        return
-    }
-    choice.classList.add('choice')
-    console.log(evt.target);
-    render();
-}
-item = () => {
-choice.dataset.name = item.name
-    let item = docume.getElementById('div')
-
-let count = 0
-if (count < 2) {
-    count++
-    if (count === 1) {
-       fact = choice.dataset.name
-       choice.classList.add('choice')
-    } else {
-        answer = choice.dataset.name
-        choice.classList.add('choice')
-    }
-    if (fact !== '' && answer !== '') {
-        if (fact === answer) {
-            set()
-        }
-    }
-}
 };
-const set = () => {
-    let choice = document.querySelectorAll('.choice')
-    choice.forEach((tile) => {
-        tile.classList.add('set')
-    })
-}
 
-function renderBoard() {
-   board.forEach((colArr, colIdx) => {
-        colArr.forEach((cellval, rowIdx) => {
-            const cellId = `c${colIdx}r${rowIdx}`;
+
+ 
+
+
+ function renderBoard() {
+    board.forEach((colArr, colIdx) => {
+         colArr.forEach((cellval, rowIdx) => {
+             const cellId = `c${colIdx}r${rowIdx}`;
             const cellEl = document.getElementById(cellId);
         });
        
         });
         
-    }
+     }
 
 
-function renderDone() {
-    if (gameComplete === 'C') {
-        messageEl.innerText = "Congratulations!" 
-    } else {
+ function renderDone() {
+     if (gameComplete === 'C') {
+         messageEl.innerText = "Congratulations!" 
+     } else {
         
 
-    }
-}
+     }
+ }
 
-//function renderControls()    {
-    //startAgainBtn.style.visibility = done ? 'visible' : 'hidden';
-//}
+function renderControls()    {
+    startAgainBtn.style.visibility = done ? 'visible' : 'hidden';
+}
 
 
 
